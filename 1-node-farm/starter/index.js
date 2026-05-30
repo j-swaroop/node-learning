a288f1fda0db30d76b2588c08d839b0167d427b4
j-swaroop <133971855+j-swaroop@users.noreply.github.com>
2026-05-30 16:06:13 +0530

    Reading and Writing Files Asynchronously

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,7 +1,18 @@
 const fs = require("fs");
 
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// // Blocking - Synchronous
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 
-const textOut = `Testing the write file ${textIn}`;
+// const textOut = `Testing the write file ${textIn}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
 
-fs.writeFileSync("./txt/output.txt", textOut);
+// NON-blocking - Asynchronous
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+        console.log("File written");
+      });
+    });
+  });
+});
