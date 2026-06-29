2e11edd345987fb62b2c23481897cb4bc521b8c7
j-swaroop <133971855+j-swaroop@users.noreply.github.com>
2026-06-29 16:48:10 +0530

    feat: created routes folder with individual routes

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -2,161 +2,16 @@ const fs = require('fs');
 const express = require('express');
 const morgan = require('morgan');
 
+const toursRouter = require('./routes/toursRoute');
+const usersRouter = require('./routes/usersRoute');
+
 const app = express();
 app.use(express.json());
 
 app.use(morgan('dev'));
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`),
-);
-
-const getAllTours = (req, res) => {
-  res.status(301).send({
-    status: 'success',
-    total: tours.length,
-    data: {
-      tours,
-    },
-  });
-};
-
-const getTour = (req, res) => {
-  const id = Number(req.params.id);
-
-  const foundIndex = tours.findIndex((item) => item.id === id);
-
-  if (foundIndex === -1) {
-    res.status(404).send({
-      status: 'failed',
-      message: 'Invalid Id',
-    });
-    return;
-  }
-
-  const tour = tours[foundIndex];
-  res.status(200).send({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
-};
-
-const createTour = (req, res) => {
-  // console.log(req.body);
-
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).send({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    },
-  );
-};
-
-const updateTour = (req, res) => {
-  const id = Number(req.params.id);
-
-  const foundIndex = tours.findIndex((item) => item.id === id);
-
-  if (foundIndex === -1) {
-    res.status(404).send({
-      status: 'failed',
-      message: 'Invalid Id',
-    });
-    return;
-  }
-
-  const tour = tours[foundIndex];
-
-  tour.name = req.body.name;
-  res.status(200).send({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
-};
-
-const deleteTour = (req, res) => {
-  const id = Number(req.params.id);
-
-  const foundIndex = tours.findIndex((item) => item.id === id);
-
-  if (foundIndex === -1) {
-    res.status(404).send({
-      status: 'failed',
-      message: 'Invalid Id',
-    });
-    return;
-  }
-
-  tours.splice(foundIndex, 1);
-  res.status(200).send({
-    status: 'success',
-    data: {
-      tour: null,
-    },
-  });
-};
-
-const getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet implemented',
-  });
-};
-
-const createUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet implemented',
-  });
-};
-
-const getUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet implemented',
-  });
-};
-
-const updateUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet implemented',
-  });
-};
-
-const deleteUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet implemented',
-  });
-};
-
-const tourRouter = express.Router();
-const userRouter = express.Router();
-
-tourRouter.route('/').get(getAllTours).post(createTour);
-tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
-
-userRouter.route('/').get(getAllUsers).post(createUser);
-userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
-
-app.use('/api/v1/tours', tourRouter);
-app.use('/api/v1/users', userRouter);
+app.use('/api/v1/tours', toursRouter);
+app.use('/api/v1/users', usersRouter);
 
 const port = 8000;
 app.listen(port, () => {
