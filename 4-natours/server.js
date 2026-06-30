77f821246ccfcb6ec4c3c8fc4f5be3e0e6b1ba59
j-swaroop <133971855+j-swaroop@users.noreply.github.com>
2026-06-30 17:06:39 +0530

    feat: connecting to database using mongoose

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,6 +1,23 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    console.log('DB Connected Successfully');
+  });
 
 const app = require('./app');
 
