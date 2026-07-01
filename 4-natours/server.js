bc75fa14c3355f1bad58d2cea452e5d11786b98f
j-swaroop <133971855+j-swaroop@users.noreply.github.com>
2026-07-01 10:20:43 +0530

    feat: creating a simple tour model and add tour schema and test tour instance

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -19,6 +19,39 @@ mongoose
     console.log('DB Connected Successfully');
   });
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A Tour Must have name.'],
+    unique: true,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A Tour Must have price'],
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park Camper',
+  rating: 4.2,
+  price: 500,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 const app = require('./app');
 
 const port = process.env.PORT || 8000;
