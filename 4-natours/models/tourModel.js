8913ec56f68928ac22406433d7030567be1034e7
j-swaroop <133971855+j-swaroop@users.noreply.github.com>
2026-07-02 18:28:03 +0530

    feat: add slug generation for tour names and include slugify dependency

--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -62,6 +63,22 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
+
+//Document Middleware: runs before save and create
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// tourSchema.pre('save', function (next) {
+//   console.log('will save document');
+//   next();
+// });
+
+// tourSchema.post('save', function (doc, next) {
+//   console.log(doc);
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
