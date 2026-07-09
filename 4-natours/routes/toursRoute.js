72748033054c7c6c3f58de940fb58eab6ca4032c
j-swaroop <133971855+j-swaroop@users.noreply.github.com>
2026-07-09 14:46:53 +0530

    feat(auth): streamline route protection and access control for users and tours

--- 4-natours/routes/toursRoute.js
+++ 4-natours/routes/toursRoute.js
@@ -13,17 +13,31 @@ const router = express.Router();
 router.use('/:tourId/reviews', reviewsRouter);
 
 router.route('/tour-stats').get(toursController.getTourStats);
-router.route(`/monthly-plan/:year`).get(toursController.getMonthlyPlan);
+router
+  .route(`/monthly-plan/:year`)
+  .get(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide', 'guide'),
+    toursController.getMonthlyPlan,
+  );
 
 router
   .route('/')
-  .get(authController.protect, toursController.getAllTours)
-  .post(toursController.createTour);
+  .get(toursController.getAllTours)
+  .post(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide', 'guide'),
+    toursController.createTour,
+  );
 
 router
   .route('/:id')
   .get(toursController.getTour)
-  .patch(toursController.updateTour)
+  .patch(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    toursController.updateTour,
+  )
   .delete(
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
